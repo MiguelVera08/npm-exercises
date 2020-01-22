5318803c793e9035bae44c521614f8caacc9e612
Miguel Vera <miguel.vera7908@gmail.com>
2020-01-22 09:36:28 -0600

    Finshed npm Exercise

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const $ = require('jquery');
+
+// let sayHello = () => {
+// //     console.log('Hello');
+// // };
+// //
+// // sayHello();
+
+$(document).ready(function () {
+    $('#message').html("Hello")
+});
